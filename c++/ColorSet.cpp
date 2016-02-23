43b3e322207eeb2bb84ca739c8d6970dd35a17cd
Jiayang (JY) Li <jli1@colgate.edu>
2016-02-23 00:07:02 -0500

    multi-threaded version using openmp

--- c++/ColorSet.cpp
+++ c++/ColorSet.cpp
@@ -5,35 +5,42 @@
 #include "ColorSet.h"
 //==============================================================
 
-ColorSet::ColorSet(int_vec_t& init_red, int_vec_t& init_blue, size_t k, size_t l)
-	: k(k-1), l(l-1), red(move(init_red)), blue(move(init_blue))	{ }
+ColorSet::ColorSet(int_vec_t& init_red, int_vec_t& init_blue, uint32_t k, uint32_t l)
+	: k(k-1), l(l-1), red(move(init_red)), blue(move(init_blue)) {
+		init_set(red, red_set);
+		init_set(blue, blue_set);
+	}
 
-int ColorSet::search(unsigned int new_diff) {
-	unsigned int p = 0, q = 0;
+uint32_t ColorSet::search(uint32_t new_diff) {
+	uint32_t p = 0, q = 0;
 
 	//========== debugging print statements ==============
 	//cout << "Trying to add: " << new_diff << endl;
-	//cout << "Coloring " << new_diff << " red in: ";
-	//this->pprint();
+	cout << "Coloring " << new_diff << " red in: ";
+	this->pprint();
 	//====================================================
 
 	// try coloring new_diff red
 	red.push_back(new_diff);
-	if (avoids(red, k)) {
+	red_set.insert(new_diff);
+	if (avoids(red, red_set, k)) {
 		p = search(new_diff+1);
 	}
+	red_set.erase(new_diff);
 	red.pop_back();
 
 	//========== debugging print statements ==============
-	//cout << "Coloring " << new_diff << " blue in: ";
-	//this->pprint();
+	cout << "Coloring " << new_diff << " blue in: ";
+	this->pprint();
 	//====================================================
 
 	// try coloring new_diff blue
 	blue.push_back(new_diff);
-	if (avoids(blue, l)) {
+	blue_set.insert(new_diff);
+	if (avoids(blue, blue_set, l)) {
 		q = search(new_diff+1);
 	}
+	blue_set.erase(new_diff);
 	blue.pop_back();
 
 	return MAX(p, q, new_diff);
@@ -49,6 +56,13 @@ void ColorSet::pprint() {
 
 
 // helper utility functions
+inline void init_set(const int_vec_t& v, int_set_t& s) {
+	for (int_vec_t::const_iterator iter = v.begin(); iter != v.end(); iter++) {
+		uint32_t n = *iter;
+		s.insert(n);
+	}
+}
+
 inline void print_color_set(const int_vec_t& color_set) {
 	cout << "{";
 	for (int_vec_t::const_iterator iter = color_set.begin(); iter != color_set.end(); iter++) {
@@ -57,28 +71,71 @@ inline void print_color_set(const int_vec_t& color_set) {
 	cout << " }";
 }
 
-inline bool avoids(const int_vec_t& color_set, size_t size) {
-	if (size > color_set.size()) {
+inline bool avoids(const int_vec_t& color_set, const int_set_t& set, uint32_t size) {
+	uint32_t n = color_set.size(); 
+
+	// easy case: not enough colors, definitely true
+	if (size > n) {
 		return true;
 	}
 
-	int subset[size];
-	return recur_avoids(size, color_set, subset, 0, 0);
-}
+	uint32_t num_combs = nchoosek(n, size);
 
-void print_arr(const int *arr, size_t size) {
-	for (unsigned int i = 0; i < size; i++) {
-		cout << arr[i] << " ";
+	// n too small, use sequential version
+	if (num_combs < THRESHOLD) {
+		uint32_t subset[size];
+		return avoids_seq(size, color_set, set, subset, 0, 0);
+	} else {	// for big n, use parallelized version
+		return avoids_para(color_set, set, size, n, num_combs);
 	}
-	cout << endl;
 }
 
-inline bool clique_exists(const int_vec_t& color_set, const int *subset, size_t k) {
+inline bool avoids_para(const int_vec_t& color_set, const int_set_t& set, uint32_t size, uint32_t n, uint32_t num_combs) {
+	bool no_clique = true;
+
+	#pragma omp parallel default(shared)
+	{
+		uint32_t subset[size+2];	// the first and last element are dummy
+		subset[0] = n;				// for eaiser comb generation
+		subset[size+1] = n;			
+
+		uint32_t id, num_threads, chunk, start, end;
+
+		// evenly spread subsets across different threads
+		id = omp_get_thread_num();
+		num_threads = omp_get_num_threads();
+		chunk = num_combs / num_threads;
+		start = id * chunk;
+		if (id == num_threads-1)
+			end = num_combs;
+		else
+			end = (id+1) * chunk;
+
+		// search for clique
+		unrank(subset, start, n, size);
+		while (no_clique) {
+			if (clique_exists_para(color_set, set, subset, size)) {
+				no_clique = false;
+			}
+			start++;
+			if (start == end)
+				break;
+			else
+				successor(subset, n, size);
+		} 
+	}
+
+	return no_clique;
+}
+
+inline bool clique_exists_para(const int_vec_t& color_set, const int_set_t& set, const uint32_t *subset, uint32_t k) {
+	// In parallel version, subset consists of indexes
 	int_vec_t::const_iterator begin = color_set.begin(), end = color_set.end();
 	
-	for (unsigned int i = 0; i < k; i++) {
-		for (unsigned int j = i+1; j < k; j++) {
-			if (!binary_search(begin, end, subset[j]-subset[i])) {
+	for (uint32_t i = 1; i <= k; i++) {
+		for (uint32_t j = i+1; j <= k; j++) {
+			if (set.find(color_set[subset[j]]-color_set[subset[i]]) == set.end()) {
+				//cout << (set.find(color_set[subset[j]]-color_set[subset[i]]) == set.end()) << endl;
 				return false;
 			}
 		}
@@ -87,33 +144,102 @@ inline bool clique_exists(const int_vec_t& color_set, const int *subset, size_t 
 	return true;
 }
 
-inline bool recur_avoids(size_t k, const int_vec_t& color_set, int *subset, 
-						unsigned int start, unsigned int i) {
+inline bool clique_exists_seq(const int_vec_t& color_set, const int_set_t& set, const uint32_t *subset, uint32_t k) {
+	// In sequential version, subset consists of actual values
+	int_vec_t::const_iterator begin = color_set.begin(), end = color_set.end();
+	
+	for (uint32_t i = 0; i < k; i++) {
+		for (uint32_t j = i+1; j < k; j++) {
+			if (set.find(subset[j]-subset[i]) == set.end()) {
+				//cout << (set.find(subset[j]-subset[i]) == set.end()) << endl;
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+inline uint32_t nchoosek(uint32_t n, uint32_t k) {
+	//take the smaller side, minimize num of operations
+	uint32_t l = n-k;
+	if (l < k)
+		k = l;
+
+	uint32_t numerator = 1, denominator = 1;
+	for (uint32_t i = 1; i < k+1; i++) {
+		numerator *= n-k+i;
+		denominator *= i;
+	}
+	return numerator/denominator;
+}
+
+inline void unrank(uint32_t *t, uint32_t r, uint32_t n, uint32_t k) {
+	// Using minimal change order
+	uint32_t x = n;
+	for (uint32_t i = k; i > 0; i--) {
+		while (nchoosek(x, i) > r) {
+			x--;
+		}
+		t[i] = x;
+		r = nchoosek(x+1, i)-r-1;
+	}
+}
+
+inline void successor(uint32_t *t, uint32_t n, uint32_t k) {
+	// Using minimal change order
+	// Generate in-place the next combination following sub_idx
+	uint32_t j = 1;
+	while (j <= k && t[j] == j-1) {
+		j++;
+	}
+
+	if ((k-j) % 2 != 0) {
+		if (j == 1) {
+			t[j] = t[j] - 1;
+		} else {
+			t[j-1] = j-1;
+			t[j-2] = j-2;
+		}
+	} else {
+		if (t[j+1] != t[j] + 1) {
+			t[j-1] = t[j];
+			t[j] = t[j] + 1;
+		} else {
+			t[j+1] = t[j];
+			t[j] = j-1;
+		}
+	}
+}
+
+inline bool avoids_seq(uint32_t k, const int_vec_t& color_set, const int_set_t& set, uint32_t *subset, 
+						uint32_t start, uint32_t i) {
 	if (i == k) {
-		return (!clique_exists(color_set, subset, k));
+		return (!clique_exists_seq(color_set, set, subset, k));
 	}
 
 	subset[i] = color_set[start];
 
 	//always include the next number
-	if (!recur_avoids(k,color_set,subset,start+1,i+1)) {
+	if (!avoids_seq(k, color_set, set, subset, start+1, i+1)) {
 		return false;
 	}
 
 	//only exclude when there're enough elements
 	if (color_set.size()-start > k-i) {
-		return recur_avoids(k,color_set,subset,start+1,i);
+		return avoids_seq(k, color_set, set, subset, start+1,i);
 	}
 
 	return true;
 }
 
 
-
+//==============================================================
+//====================== main function =========================
 int main() {
 	int_vec_t red, blue;
 	red.push_back(1);
-	ColorSet cs (red, blue, 3, 9);
+	ColorSet cs (red, blue, 4, 7);
 
 	cout << cs.search(2) << endl; 
 }
--- c++/ColorSet.h
+++ c++/ColorSet.h
@@ -4,30 +4,41 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <unordered_set>
+#include <omp.h>
 
 #define MAX(a, b, c) ((a>b)?((a>c)?a:c):((b>c)?b:c))
+#define THRESHOLD 50
 
 using namespace std;
-typedef vector<int> int_vec_t;
+typedef vector<uint32_t> int_vec_t;
+typedef unordered_set<uint32_t> int_set_t;
 //==============================================================
 
 class ColorSet {
 private:
-	size_t k, l;
+	uint32_t k, l;
 	int_vec_t red, blue;
+	int_set_t red_set, blue_set;
 
 public:
-	ColorSet(int_vec_t& init_red, int_vec_t& init_blue, size_t k, size_t l);
-	int search(unsigned int new_diff);
+	ColorSet(int_vec_t& init_red, int_vec_t& init_blue, uint32_t k, uint32_t l);
+	uint32_t search(uint32_t new_diff);
 	void pprint();
 };
 
 
 // helper utility functions 
+inline void init_set(const int_vec_t& v, int_set_t& s);
 inline void print_color_set(const int_vec_t& color_set);
-inline bool avoids(const int_vec_t& color_set, size_t size);
-inline bool clique_exists(const int_vec_t& color_set, const int *subset, size_t k);
-inline bool recur_avoids(size_t k, const int_vec_t& color_set, int *subset, 
-						unsigned int start, unsigned int i);
+inline bool avoids(const int_vec_t& color_set, const int_set_t& set, uint32_t size);
+inline bool clique_exists_para(const int_vec_t& color_set, const int_set_t& set, const uint32_t *subset, uint32_t k);
+inline bool clique_exists_seq(const int_vec_t& color_set, const int_set_t& set, const uint32_t *subset, uint32_t k);
+inline uint32_t nchoosek(uint32_t n, uint32_t k);
+inline void unrank(uint32_t *t, uint32_t r, uint32_t n, uint32_t k);
+inline void successor(uint32_t *t, uint32_t n, uint32_t k);
+inline bool avoids_seq(uint32_t k, const int_vec_t& color_set, const int_set_t& set, uint32_t *subset, 
+						uint32_t start, uint32_t i);
+inline bool avoids_para(const int_vec_t& color_set, const int_set_t& set, uint32_t size, uint32_t n, uint32_t num_combs);
 
 #endif
